df9285c75e896fe1e9a3c4b5009160446a988943
José Henrique da Silva <68820356+jshenriquedf@users.noreply.github.com>
2023-01-18 10:15:26 -0300

    Create fire.sh

---
+++ admin/fire.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Este script é chamado por /etc/network/interfaces
+# v20230118 by Henrique
+
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+iptables -F
+iptables -t nat -F
+
+########################
+### Segurança básica ###
+########################
+
+# Regra
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp -m tcp ! --tcp-flags FIN,SYN,RST,ACK SYN -m state --state NEW -j DROP
+iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,SYN,RST,PSH,ACK,URG NONE -j DROP
+iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,SYN FIN,SYN -j DROP
+iptables -A INPUT -p tcp -m tcp --tcp-flags SYN,RST SYN,RST -j DROP
+iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,RST FIN,RST -j DROP
+iptables -A INPUT -p tcp -m tcp --tcp-flags FIN,ACK FIN -j DROP
+iptables -A INPUT -p tcp -m tcp --tcp-flags ACK,URG URG -j DROP
+iptables -A INPUT -m state --state INVALID -j DROP
+iptables -A INPUT -d 224.0.0.0/32 -j REJECT --reject-with icmp-port-unreachable
+iptables -A INPUT -p tcp -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p udp -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p icmp -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p icmp -m state --state NEW,ESTABLISHED -m icmp --icmp-type 8 -j ACCEPT
+iptables -A INPUT -m state --state NEW -j IN-NEW
+iptables -A INPUT -j LOG --log-prefix "IPT_INPUT: " --log-level 6
+iptables -A INPUT -j DROP
+iptables -A FORWARD -j LOG --log-prefix "IPT_FORWARD: " --log-level 6
+iptables -A FORWARD -j DROP
+iptables -A OUTPUT -o lo -j ACCEPT
+iptables -A OUTPUT -p tcp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p udp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p icmp -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -j LOG --log-prefix "IPT_OUTPUT: " --log-level 6
+iptables -A OUTPUT -j DROP
+
+# liberar acesso SSH para a organização
+iptables -A IN-NEW -s 192.168.56.0/24 -p tcp -m tcp --dport 22 --tcp-flags FIN,SYN,RST,ACK SYN -j ACCEPT
+
+# liberar acesso ao MISP para a organização
+-A IN-NEW -s 192.168.56.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -m multiport --dports 80,443 -j ACCEPT
